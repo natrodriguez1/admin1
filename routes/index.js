6fa0491de21833ee0545e790b2083df3adb44ccc
natalie <ntrodriguezch@hotmail.com>
2025-03-20 23:31:31 -0500

    conexión con proyecto mariadb

--- routes/index.js
+++ routes/index.js
@@ -16,4 +16,34 @@ router.get('/photos', async function(req, res, next) {
   res.render('fotos', { title: 'Fotos', fotos: response.data });
 });
 
+router.get('/photos/add', function(req, res, next) {
+  res.render('fotos_formulario', { title: 'Express' });
+});
+
+router.post('/photos/save', async function(req,
+  res, next) {
+    let { title, description, rate } = req.body
+    const URL =
+    'http://localhost:4444/rest/fotos/save'
+    let data = {
+      titulo:title,
+      descripcion: description,
+      calificacion: rate,
+      ruta: ''
+    }
+    const config = {
+      proxy: {
+      host: 'localhost',
+      port: 4444
+    }
+    }
+    const response = await axios.post(URL, data,
+    config);
+    if(response.status == '200' &&
+      response.statusText == 'OK') {
+      res.redirect('/photos')
+    } else {
+      res.redirect('/')
+    }
+});
 module.exports = router;
